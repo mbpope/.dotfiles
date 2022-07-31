88e9f114f08c3f920144daa8437a602e124636d4
Micah Pope <mpope316@gmail.com>
2022-07-31 13:07:43 -0700

    I forgot what I added this time lol

--- dotconf/config/polybar/launch.sh
+++ dotconf/config/polybar/launch.sh
@@ -5,7 +5,21 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-echo "Polybar launched..."
+screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
+
+if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar main &
+else
+  primary=$(xrandr --query | grep primary | cut -d" " -f1)
+
+  for m in $screens; do
+    if [[ $primary == $m ]]; then
+        MONITOR=$m TRAY_POS=right polybar main &
+    else
+        MONITOR=$m TRAY_POS=none polybar secondary &
+    fi
+  done
+fi
